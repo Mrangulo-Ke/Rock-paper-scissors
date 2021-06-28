ac36ff1c49e0f216906f6533bf8c4515cfff52d0
Mrangulo-Ke <mrangulowanj@gmail.com>
2021-06-28 07:49:10 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ let playerScore = 0
 let computerScore = 0
 
 const buttons = document.querySelectorAll('input')
-
+const winMsg = document.querySelector('.winner');
+const result = document.getElementById('result').innerHTML
 
 function computerPlay(){
     let choices = ['rock', 'paper', 'scissors']
@@ -12,34 +13,37 @@ function computerPlay(){
 /*the game logic (who wins and who loses)*/
 function playRound(playerSelection, computerSelection) {
     let computerSelection = computerPlay
-    let result = ''
-
-    if (computerSelection === 'Rock') {
-        if (playerSelection === 'Scissors') {
-            result = 'You Lose Rock beats Scissors' ;
-        } else if (playerSelection === 'Paper') {
-            result = 'You Win Paper beats Rock'
-        } else {
-            result = 'Its a tie'
-        }
-    } else if (computerSelection === 'Paper') {
-        if (playerSelection === 'Scissors') {
-            result = 'You Win Scissors beats Paper' ;
-        } else if (playerSelection === 'Rock') {
-            result = 'You Lose Paper beats Rock'
-        } else {
-            result = 'Its a tie'
-        }
-    } else if (computerSelection === 'Scissors') {
-        if (playerSelection === 'Rock') {
-            result = 'You Win Rock beats Scissors' ;
-        } else if (playerSelection === 'Paper') {
-            result = 'You Lose Scissors beats Paper'
-        } else {
-            result = 'Its a tie'
-        }
-        
+    let result = ""
+    
+    if (playerSelection === computerSelection) {
+        result = "Its a Draw"
+        return
     }
-    document.getElementById('result').innerHTML = result
-    return result
+    if (playerSelection === paper) {
+        if (computerSelection == rock) {
+            result = "You Win"
+            return
+        }
+        else
+            result = "You Lose" 
+            return
+    }
+    if (playerSelection === rock) {
+        if (computerSelection == scissors) {
+            result = "You Win"
+            return
+        }
+        else
+            result = "You Lose"
+            return 
+    }
+    if (playerSelection === scissors) {
+        if (computerSelection == rock) {
+            result = "You Lose"
+            return
+        }
+        else
+            result = "You Win" 
+            return
+    }
 }
